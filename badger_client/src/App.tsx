7633166757e0815b549c9dd886a49a86817ebe49
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-05-31 11:11:54 -0400

    feat: Switch to React for UI

--- badger_client/src/App.tsx
+++ badger_client/src/App.tsx
@@ -1,24 +1,37 @@
-import { createSignal } from "solid-js";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
+    setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
-    <div class="p-10">
-      <h1 class="bg-blue-500">Welcome to Tauri!</h1>
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
 
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
-        class="row"
+        className="row"
         onSubmit={(e) => {
           e.preventDefault();
           greet();
@@ -32,7 +45,7 @@ function App() {
         <button type="submit">Greet</button>
       </form>
 
-      <p>{greetMsg()}</p>
+      <p>{greetMsg}</p>
     </div>
   );
 }
--- badger_client/vite.config.ts
+++ badger_client/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [solid()],
+  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
