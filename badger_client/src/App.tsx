ac88b95c5b9ee2afeeec834a951dc66264d62295
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-05-30 20:26:43 -0400

    Edit ui

--- badger_client/src/App.tsx
+++ badger_client/src/App.tsx
@@ -1,5 +1,4 @@
 import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
@@ -13,20 +12,8 @@ function App() {
   }
 
   return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
+    <div class="p-10">
+      <h1 class="bg-blue-500">Welcome to Tauri!</h1>
 
       <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
 
