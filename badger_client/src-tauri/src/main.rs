a536962a2832ac120387eb253313ebfeb995efe8
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-06-08 20:00:02 -0400

    Update main.rs

--- badger_client/src-tauri/src/main.rs
+++ badger_client/src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![deny(clippy::all)]
 
 use badger_client::types::{self, CurrentServer, Error, ServerList};
 use tauri::{Manager, State};
